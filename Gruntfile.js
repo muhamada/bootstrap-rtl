e7a5566e3ebf14461b6b12f0b85c183f3e2c0c26
Muhamad A. <m@muhamad.me>
2014-12-25 21:43:26 +0200

    grunt-contrib-copy

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,15 +1,26 @@
 /*global module:false*/
 module.exports = function(grunt) {
 
-  // Project configuration.
-  grunt.initConfig({
-    // Task configuration.
+	// Project configuration.
+	grunt.initConfig({
+		// Task configuration.
+		copy : {
+			main : {
+				files : [ {
+					expand : true,
+					cwd : 'bootstrap/',
+					src : [ 'css/**', 'components/**', 'examples/**',
+							'javascript/**', 'assets/**', 'dist/**' ],
+					dest : '.'
+				} ]
+			}
+		}
+	});
 
-  });
+	// These plugins provide necessary tasks.
+	grunt.loadNpmTasks('grunt-contrib-copy');
 
-  // These plugins provide necessary tasks.
-
-  // Default task.
-  grunt.registerTask('default', []);
+	// Default task.
+	grunt.registerTask('default', ['copy']);
 
 };
